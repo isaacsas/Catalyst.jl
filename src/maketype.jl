f77d95522fe897ac9007cb3d356fd89d81f98d2a
Samuel Isaacson <isaacsas@users.noreply.github.com>
2019-01-08 15:01:49 -0500

    more timing

--- src/maketype.jl
+++ src/maketype.jl
@@ -71,6 +71,62 @@ function maketype(name,
     typeex,constructorex
 end
 
+function maketype(name,
+    f,
+    f_func,
+    f_symfuncs,
+    jumps,
+    regular_jumps,
+    jump_rate_expr,
+    jump_affect_expr,
+    syms;
+    params = Symbol[],
+    reactions=Vector{ReactionStruct}(undef,0)
+    )
+
+typeex = :(mutable struct $name <: DiffEqBase.AbstractReactionNetwork
+f::Function
+f_func::Vector{Expr}
+f_symfuncs::Matrix{SymEngine.Basic}
+jumps::Tuple{DiffEqJump.AbstractJump,Vararg{DiffEqJump.AbstractJump}}
+regular_jumps::RegularJump
+jump_rate_expr::Tuple{Any,Vararg{Any}}
+jump_affect_expr::Tuple{Vector{Expr},Vararg{Vector{Expr}}}
+syms::Vector{Symbol}
+params::Vector{Symbol}
+reactions::Vector{ReactionStruct}
+end)
+# Make the default constructor
+constructorex = :($(name)(;
+    $(Expr(:kw,:f,f)),
+    $(Expr(:kw,:f_func,f_func)),
+    $(Expr(:kw,:jumps,jumps)),
+    $(Expr(:kw,:regular_jumps,regular_jumps)),
+    $(Expr(:kw,:jump_rate_expr,jump_rate_expr)),
+    $(Expr(:kw,:jump_affect_expr,jump_affect_expr)),
+    $(Expr(:kw,:f_symfuncs,f_symfuncs)),
+    $(Expr(:kw,:syms,syms)),
+    $(Expr(:kw,:params,params)),
+    $(Expr(:kw,:reactions,reactions))) =
+    $(name)(
+        f,
+        f_func,
+        f_symfuncs,
+        jumps,
+        regular_jumps,
+        jump_rate_expr,
+        jump_affect_expr,
+        syms,
+        params,
+        reactions
+        )) |> esc
+
+        #f_funcs,symfuncs,pfuncs,syms,symjac) |> esc
+
+# Make the type instance using the default constructor
+typeex,constructorex
+end
+
 # short version
 function maketype(name,
     syms;
